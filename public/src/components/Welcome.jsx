eb8b0000caaf699ce1afebbb8cc8b8cea30bc8e8
Atharva Tarlekar <atharvatarlekar162001@gmail.com>
2023-07-27 14:17:30 +0530

    uploading entire project from local

---
+++ public/src/components/Welcome.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Messages from "../assets/messages.gif";
+export default function Welcome() {
+  const [userName, setUserName] = useState("");
+  useEffect(async () => {
+    setUserName(
+      await JSON.parse(
+        localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+      ).username
+    );
+  }, []);
+  return (
+    <Container>
+      <img src={Messages} alt="" />
+      
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  flex-direction: column;
+  img {
+    height: 20rem;
+  }
+  span {
+    color: #4e0eff;
+  }
+`;
